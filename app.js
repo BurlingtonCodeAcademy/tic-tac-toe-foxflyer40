4120a2abcc256a17ceba0688e3fbef0a4bad770c
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-03-02 14:09:15 -0500

    deploy

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 5000 
 
 app.use(express.static('public')) //bind to public folder
 
- // set path to About page more specific
-app.get('/about', (request, response) => {
-      response.sendFile(path.resolve('./public/about.html'))
-})
-
-//less specific error catch all
-app.get('*', (req,res) => {
-      res.status(404);
-      res.sendFile(path.resolve('./public/404.html'))
-      })
 
 
 app.listen(PORT, () => {console.log(`Running port: ${PORT}`)})
